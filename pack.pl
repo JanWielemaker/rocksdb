e6caf9851904b95bd56f900bd63b42f31f746e64
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-11-25 09:42:50 +0100

    Updated version and email addresses

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(rocksdb).
 title('SWI-Prolog interface to RocksDB').
-version('0.11.0').
+version('0.12.0').
 keywords([database, embedded, nosql]).
-author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
-packager( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
-maintainer( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
+author('Jan Wielemaker', 'jan@swi-prolog.org').
+packager( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
+maintainer( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
 home( 'https://github.com/JanWielemaker/rocksdb' ).
 download('https://github.com/JanWielemaker/rocksdb.git').
