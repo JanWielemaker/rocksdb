4d49cf9e06b22f103337a1aa453cc676792f9fe0
Peter Ludemann <peter.ludemann@gmail.com>
2023-12-07 11:38:27 -0800

    Bump version #

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(rocksdb).
 title('SWI-Prolog interface to RocksDB').
-version('0.13.1').
+version('0.13.2').
 keywords([database, embedded, nosql, 'RocksDB']).
 author('Jan Wielemaker', 'jan@swi-prolog.org').
 packager( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
