47fbfdd15d51b0998b3219f24a419eb89a5f383b
Peter Ludemann <peter.ludemann@gmail.com>
2024-07-05 15:22:21 -0700

    Sync to rocksdb v9.3.1, bump version to 0.14.3

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(rocksdb).
 title('SWI-Prolog interface to RocksDB').
-version('0.14.2').
-% prolog_version('9.3.6').
+version('0.14.3').
+% prolog_version('9.3.8').
 pack_version(2).
 keywords([database, embedded, nosql, 'RocksDB']).
 author('Jan Wielemaker', 'jan@swi-prolog.org').
